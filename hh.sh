cb15ae30ecb3f2b0caaf59ba5c11dfac703149b7
HACKTERMUX <57942737+HACKTERMUX@users.noreply.github.com>
2019-11-19 18:21:45 +0700

    Add files via upload

---
+++ hh.sh
@@ -0,0 +1,86 @@
+toilet -f slant --gay "SPAM"
+sleep 1
+toilet -f slant --gay "MODE on"
+sleep 1
+
+echo "[+]----------------------------------[+]"
+echo "Coder : @mackenzie"
+echo "email : vhorsx@gmail.com"
+echo "Special Thanks to : Ricko En"
+echo "[+]----------------------------------[+]"
+sleep 5
+
+read -p "masukan nama tumbal : " nama
+sleep 1
+echo "analizing...."
+sleep 10
+echo "Found Player!"
+sleep 5
+echo "Spamming..."
+sleep 2
+
+read -p "are you sure? ($nama)? (Y/N) : " na
+if [ $na = "y" ];
+then
+    echo "$nama, Deleting protection.."
+    sleep 5
+elif [ $na = "n" ];
+then
+    exit
+    sleep 2
+elif [ $na = "Y" ];
+then
+    echo "$nama, Deleting protection.."
+    sleep 5
+elif [ $na = "N" ];
+then
+    exit
+    sleep 2
+else
+    echo "please only enter Y/N !"
+    sleep 2
+    exit
+fi
+
+echo -n "Jumlah Spam : "
+read spamz;
+
+sleep 2
+
+echo "SENDING PACK TO 127.0.0.1 Port 80"
+echo "SENDING PACK TO 127.0.0.1 Port 80"
+sleep 2
+echo "SENDING PACK TO 127.0.0.1 Port 80"
+echo "SENDING PACK TO 127.0.0.1 Port 80"
+echo "SENDING PACK TO 127.0.0.1 Port 80"
+echo "SENDING PACK TO 127.0.0.1 Port 80"
+echo "SENDING PACK TO 127.0.0.1 Port 80"
+echo "SENDING PACK TO 127.0.0.1 Port 80"
+echo "SENDING PACK TO 127.0.0.1 Port 80"
+echo "SENDING PACK TO 127.0.0.1 Port 80"
+echo "SENDING PACK TO 127.0.0.1 Port 80"
+echo "SENDING PACK TO 127.0.0.1 Port 80"
+echo "SENDING PACK TO 127.0.0.1 Port 80"
+echo "SENDING PACK TO 127.0.0.1 Port 80"
+echo "SENDING PACK TO 127.0.0.1 Port 80"
+sleep 2
+echo "SENDING PACK TO 127.0.0.1 Port 80"
+echo "SENDING PACK TO 127.0.0.1 Port 80"
+echo "SENDING PACK TO 127.0.0.1 Port 80"
+echo "SENDING PACK TO 127.0.0.1 Port 80"
+echo "SENDING PACK TO 127.0.0.1 Port 80"
+echo "SENDING PACK TO 127.0.0.1 Port 80"
+echo "SENDING PACK TO 127.0.0.1 Port 80"
+echo "SENDING PACK TO 127.0.0.1 Port 80"
+echo "SENDING PACK TO 127.0.0.1 Port 80"
+echo "SENDING PACK TO 127.0.0.1 Port 80"
+echo "SENDING PACK TO 127.0.0.1 Port 80"
+echo "SENDING PACK TO 127.0.0.1 Port 80"
+echo "SENDING PACK TO 127.0.0.1 Port 80"
+echo "SENDING PACK TO 127.0.0.1 Port 80"
+echo "SENDING PACK TO 127.0.0.1 Port 80"
+sleep 10
+echo "done,sending $spamz messages!"
+sleep 10
+toilet -f slant --gay "DONE ! "
+sleep 5
